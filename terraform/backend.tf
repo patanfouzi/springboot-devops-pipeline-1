9b7aa5a557c418ff87f262408a6e362bb088a05c
mehziya0352 <mehziya0352@gmail.com>
2025-10-06 19:58:44 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "my-slurm-state-bucket"
     key    = "secure-devops-pipeline-minimal/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraform-locks"
     encrypt = true
   }
 }
