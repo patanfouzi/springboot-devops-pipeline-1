1a9e82297342f023b86bf8f48541764fd82a491b
mehziya0352 <mehziya0352@gmail.com>
2025-10-07 23:56:50 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "app_public_ip" {
-  description = "Public IP of the App server"
-  value       = aws_instance.app.public_ip
+  value = aws_instance.app.*.public_ip
 }
 
 output "mysql_public_ip" {
-  description = "Public IP of the MySQL server"
-  value       = aws_instance.mysql.public_ip
+  value = aws_instance.mysql.*.public_ip
 }
