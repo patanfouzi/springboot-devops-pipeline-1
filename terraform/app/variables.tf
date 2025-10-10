5aa2b201e0c3bbb9a1f3c1533b81599b09dfb613
mehziya0352 <mehziya0352@gmail.com>
2025-10-10 16:57:27 +0530

    Create variables.tf

---
+++ terraform/app/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Existing AWS key pair name (must exist in AWS)"
+}
