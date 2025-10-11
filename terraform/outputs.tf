7b1ebabcc058295ba12fc32fca86e9891e002e7c
mehziya0352 <mehziya0352@gmail.com>
2025-10-11 20:16:20 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,6 @@ output "mysql_public_ip" {
   description = "Public IP of the Mysql server"
   value = aws_instance.mysql.*.public_ip
 }
+output "mysql_private_ip" {
+  value = aws_instance.mysql.*.private_ip
+}
